2b914d84849d3a7da77ce95298064d1953e126f4
Jacob Errington <git@mail.jerrington.me>
2026-04-18 19:49:54 -0400

    run git pull on startup

--- nether_portal/__init__.py
+++ nether_portal/__init__.py
@@ -1,6 +1,11 @@
+import subprocess
+import os
+
 from flask import Flask
 
 from . import hevy, strava
+
+subprocess.run(['git', '-C', os.environ['OBSIDIAN_VAULT_PATH'], 'pull', '--rebase'], check=True)
 
 app = Flask(__name__)
 app.register_blueprint(hevy.bp)
